06de00bc80d0030c7e2c49510064815c953512ff
Siarhei Beliakou <sbeliakou@gmail.com>
2018-09-07 07:40:56 +0300

    config.rb

--- config.rb
+++ config.rb
@@ -1,16 +1,22 @@
 # If you change, Keep the structure with the dot. [0-9 a-f]
-$token = "12345f.0123456789abcdef"
+$token = "abcdef.0123456789abcdef"
+
 # Total memory of master
 $master_memory = 2560
+
 # Increment to have more nodes
 $worker_count = 0
+
 # Total memory of nodes
 $worker_memory = 1536
+
 # Add Grafana with InfluxDB (work with heapster)
-$grafana = true
+$grafana = false
+
 # Deploy Ingress Controller
 $ingress_controller = true
-# Local Domain Name
-$domain = "devops.kubernetes.my"
+
 # Deploy Prometheus
-$prometheus = false
+$prometheus = true
+$prometheus_fqdn = "prometheus.example.com"
+$grafana_fqdn = "grafana.example.com"
