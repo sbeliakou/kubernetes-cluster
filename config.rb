8f5c05b58a43d61c88acad6dacab0f04a0c5cead
Siarhei Beliakou <sbeliakou@gmail.com>
2018-12-29 01:04:46 +0300

    metallb

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ $token = "abcdef.0123456789abcdef"
 $master_memory = 2560
 
 # Increment to have more nodes
-$worker_count = 0
+$worker_count = 1
 
 # Total memory of nodes
 $worker_memory = 1536
@@ -13,10 +13,15 @@ $worker_memory = 1536
 # Add Grafana with InfluxDB (work with heapster)
 $grafana = false
 
+# Cluster IP Addresses
+$cluster_ips = "192.168.56.224/28"
+$metallb_ips = "192.168.56.240/28"
+
+# Deploy Metal LB
+$metallb = true
+
 # Deploy Ingress Controller
 $ingress_controller = true
 
 # Deploy Prometheus
 $prometheus = true
-$prometheus_fqdn = "prometheus.example.com"
-$grafana_fqdn = "grafana.example.com"
