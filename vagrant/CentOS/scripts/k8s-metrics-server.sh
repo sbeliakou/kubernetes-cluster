e2fb1ba98b8a06b7b50b7d866907630d25d787fe
Siarhei Beliakou <sbeliakou@gmail.com>
2019-06-30 17:19:32 +0300

    metrics-server

---
+++ vagrant/CentOS/scripts/k8s-metrics-server.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+cat <<END
+Executing ${0}
+================================================================================
+
+    Installing Metrics Server
+
+    - https://github.com/kubernetes-incubator/metrics-server/blob/master/README.md
+    - https://github.com/linuxacademy/metrics-server
+
+================================================================================
+
+END
+
+yum install -y git
+
+cd /tmp/
+git clone https://github.com/kubernetes-incubator/metrics-server.git
+cd metrics-server
+
+kubectl create -f deploy/1.8+/
+kubectl patch deployment/metrics-server -n kube-system --patch '{
+  "spec": {
+    "template": {
+      "spec": {
+        "containers": [
+          {
+            "name": "metrics-server", 
+            "command":[
+              "/metrics-server", 
+              "--kubelet-preferred-address-types=InternalIP,ExternalIP,Hostname",
+              "--kubelet-insecure-tls"
+            ]
+          }
+        ]
+      }
+    }
+  }
+}'
+
+rm -rf metrics-server
