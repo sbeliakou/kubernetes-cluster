c392d52222b808c8bb7f31a0ab51abc251359d3e
Siarhei Beliakou <sbeliakou@gmail.com>
2019-06-30 17:25:18 +0300

    k8s version

--- config.rb
+++ config.rb
@@ -4,14 +4,14 @@ $token = "abcdef.0123456789abcdef"
 # Total memory of master
 $master_memory = 6000
 
-# Increment to have more nodes
-$worker_count = 0
+# Worker Nodes, 0 = Master Isolation
+$worker_count = 2
 
 # Total memory of nodes
 $worker_memory = 1536
 
-# Add Grafana with InfluxDB (work with heapster)
-$grafana = false
+# Kubernetes Version
+$k8s_version = "1.13.7"
 
 # Cluster IP Addresses
 $cluster_ips = "192.168.56.224/28"
@@ -25,3 +25,6 @@ $ingress_controller = "nginx"
 
 # Deploy Prometheus
 $prometheus = false
+
+# Grafana with InfluxDB (work with heapster)
+$grafana = false
