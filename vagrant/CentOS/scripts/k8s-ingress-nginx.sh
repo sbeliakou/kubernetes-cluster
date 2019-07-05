bdfa78d98b0c440ba45f15b33d5b54f0bd986542
Siarhei Beliakou <sbeliakou@gmail.com>
2019-07-05 20:34:31 +0300

    updated ingress nginx

--- vagrant/CentOS/scripts/k8s-ingress-nginx.sh
+++ vagrant/CentOS/scripts/k8s-ingress-nginx.sh
@@ -6,42 +6,33 @@ Executing ${0}
 
     Installing Ingress Controller
 
-    - https://github.com/nginxinc/kubernetes-ingress/blob/master/docs/installation.md
+    - https://github.com/kubernetes/ingress-nginx/blob/master/docs/deploy/index.md
+    - https://github.com/kubernetes/ingress-nginx/blob/master/docs/deploy/baremetal.md
     - https://kubernetes.github.io/ingress-nginx/deploy/#bare-metal
 
 ================================================================================
 
 END
 
-yum install -y git
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/mandatory.yaml
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/cloud-generic.yaml
 
-cd /tmp/
-git clone https://github.com/nginxinc/kubernetes-ingress.git
-git checkout v1.5.0
-cd kubernetes-ingress/deployments
-
-kubectl apply -f common/ns-and-sa.yaml
-kubectl apply -f common/default-server-secret.yaml
-kubectl apply -f common/nginx-config.yaml
-kubectl apply -f rbac/rbac.yaml
-kubectl apply -f deployment/nginx-ingress.yaml
+version="nginx-0.24.1"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/${version}/deploy/mandatory.yaml
 
 is_metallb=${1}
 
 if [ "${is_metallb}" == "true" ]; then
-  kubectl apply -f service/loadbalancer.yaml
+  kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/${version}/deploy/provider/cloud-generic.yaml
 else
-  kubectl apply -f service/nodeport.yaml
+  kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/${version}/deploy/provider/baremetal/service-nodeport.yaml
   IPADDR=$(kubectl get node -l node-role.kubernetes.io/master -o jsonpath='{.items[0].status.addresses[?(@.type=="InternalIP")].address}')
-  kubectl patch svc ingress-nginx -n nginx-ingress --patch '{ "spec": {"externalIPs": [ "'${IPADDR}'" ] }}'
+  kubectl patch svc ingress-nginx -n ingress-nginx --patch '{ "spec": {"externalIPs": [ "'${IPADDR}'" ] }}'
 fi
 
-while [ "$(kubectl get pod -n nginx-ingress -o jsonpath='{.items[0].status.phase}')" != "Running" ]; do
+while [ "$(kubectl get pod -n ingress-nginx -o jsonpath='{.items[0].status.phase}')" != "Running" ]; do
   echo $(date +"[%H:%M:%S]") Nginx Ingress Controller not Ready
   sleep 10
 done
 
-kubectl get pods --all-namespaces -l app==nginx-ingress
-kubectl get svc -n nginx-ingress
-
-rm -rf kubernetes-ingress/deployments
+kubectl get svc -n ingress-nginx
