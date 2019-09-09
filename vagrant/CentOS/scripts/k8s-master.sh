9e3aeb64857b1dd147c5b4113a4d3846cba71aaa
Siarhei Beliakou <sbeliakou@gmail.com>
2019-09-09 20:55:52 +0300

    flannel

--- vagrant/CentOS/scripts/k8s-master.sh
+++ vagrant/CentOS/scripts/k8s-master.sh
@@ -7,7 +7,6 @@ Executing ${0}
     https://kubernetes.io/docs/setup/independent/create-cluster-kubeadm/
 
     Configuring Kubernetes Master:
-      - cluster init
       - Pod network: Flannel
 
 ================================================================================
@@ -34,44 +33,4 @@ if [ ! -e /etc/kubernetes/kubelet.conf ]; then
         rm -rf /vagrant/.kube
         /bin/cp -rf $HOME/.kube /vagrant/.kube
     fi
-
-    export KUBECONFIG=/etc/kubernetes/admin.conf
-
-    # Installing a Pod Network
-    # kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
-    # https://github.com/coreos/flannel/blob/master/Documentation/troubleshooting.md#vagrant
-    IPETHX=$(ip r | grep $(hostname -I | sed 's/10.0.2.15//' | awk '{print $1}') | cut -d' ' -f3)
-    # kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
-    kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.11.0/Documentation/kube-flannel.yml
-    kubectl patch daemonsets kube-flannel-ds-amd64 -n kube-system --patch '{
-        "spec": {
-            "template": {
-                "spec": {
-                    "containers": [
-                        {
-                            "name": "kube-flannel", 
-                            "args": [
-                                "--ip-masq",
-                                "--kube-subnet-mgr",
-                                "--iface='${IPETHX}'"
-                            ]
-                        }
-                    ]
-                }
-            }
-        }
-    }'
-
-    kubectl get daemonsets -n kube-system kube-flannel-ds-amd64
-
-    # Wait until Master is Up
-    while kubectl get nodes | grep master | grep NotReady >/dev/null;
-    do
-        echo $(date +"[%H:%M:%S]") Master is not ready yet
-        sleep 10
-    done
-
-    echo $(date +"[%H:%M:%S]") Master is in Ready mode
 fi
-
-kubectl get nodes
